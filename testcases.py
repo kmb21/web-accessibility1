b4a558aa1051b38f88a903d5301dfd553f78556a
kmb21 <113995857+kmb21@users.noreply.github.com>
2023-11-05 18:55:04 -0500

    Updated Test cases

--- testcases.py
+++ testcases.py
@@ -7,3 +7,6 @@ class TestSoup(unittest.TestCase):
     def setUp(self): #Creates a soup object
         self.soup = Soup() #think of a way to not use a filepath but still use the constructor
         
+        
+    def test_head_Tag(self):
+        
